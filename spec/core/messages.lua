b3494fd1e4e13f2ccb2d6d43567b16abf22dee63
Andrew Starks <andrew.starks@trms.com>
2014-06-23 17:03:28 -0500

    updated message tests, as per discussion.

--- spec/core/messages.lua
+++ spec/core/messages.lua
@@ -21,18 +21,43 @@ local pair_2 = nml.socket(AF_SP, NN_PAIR)
 nml.connect(pair_2, PAIR_ADDR)
 local msg_ud, msg_str
 
+local message_api = {
+ 	"nml_msg", 		-- make a new message
+ 	"settype", 		-- set the type of message
+ 	"gettype", 		-- get the type of message
+ 	"fromstring", 	--make message contents from a string
+ 	"tostring", 	--turn any message into a Lua string.
+ 	"getsize", 		--number of bytes in a message. nil is no message present (not initialized).
+ 	"free", 		--free an existing message. 
+ 						--true is "message freed", 
+ 						--false is "no message present", 
+ 						--nil, err is an error.
+ 	"alloc",		--allocates memore for a buffer. probably only called in C
+ 	"realloc", 		--dito, but reaollocates...
+ 	--not sure what other memory functions are needed...
+}
+describe("the nml_msg api methods #nml_msg", function()
+	
+		for i, method in ipairs(message_api) do
+			it("has all of the api calls present.", function()
+				assert.is_equal("function", type(nml[message_api]))
+			end)
+		end
 
-describe ("nml_msg messages #nml_msg", function()
+
+
+end)
+describe ("basic nml_msg operation #nml_msg", function()
 	it("can make a new message that is empty.", function()
 		--new empty message
-		msg_ud = nml.new_message()
+		msg_ud = nml.nml_msg()
 
 		assert.is_equal("userdata", type(msg_ud))
 		--this is so that I can see what type it is, in unadorned Lua
 		--also note that colon syntax works here too. I'll use that later.
-		assert.is_equal("nml_msg", nml.type(msg_ud))
+		-- assert.is_equal("nml_msg", nml.type(msg_ud))
 		-- has not been set yet.
-		assert.is_equal(nil, nml.msg_type(msg_ud))
+		assert.is_equal(nil, nml.gettype(msg_ud))
 
 		--this should call the __gc method, which should see that the buffer is nil and do nothing.
 		--don't know how to test that though.
@@ -43,7 +68,7 @@ describe ("nml_msg messages #nml_msg", function()
 
 	it("can make a new empty message and then have a string serialized into it.", function()
 		--new empty message
-		msg_ud = nml.new_message() 
+		msg_ud = nml.nml_msg() 
 		msg_str ="Hello, World!" 
 
 	--I can do this, if needed: no type specified. imply string.
@@ -52,7 +77,7 @@ describe ("nml_msg messages #nml_msg", function()
 		
 		--set automatically if absent.
 
-		assert.is_equal("string", nml.msg_type(msg_ud))
+		assert.is_equal("string", nml.gettype(msg_ud))
 		
 		it("will let me re-make the buffer in an existing message.", function()
 			assert.is_truthy(nml.fromstring(msg_ud, "My second string"))
@@ -60,11 +85,19 @@ describe ("nml_msg messages #nml_msg", function()
 		end) 
 
 	end)
-	it("will let me specifiy the type, explicitly.", function()
+
+	it("will let me set the type, explicitly.", function()
+		assert.is_truthy(nml.settype(msg_ud, "bar_string"))
+
+		assert.is_equal("bar_string", nml.gettype(msg_ud))
+	end)
+
+	it("will let me specifiy the type, explicitly, when I make a message from a string.", function()
 		assert.is_truthy(nml.fromstring(msg_str, "foo_string"))
 
-		assert.is_equal("foo_string", nml.msg_type(msg_ud))
+		assert.is_equal("foo_string", nml.gettype(msg_ud))
 	end)
+
 
 	it("can reproduce the message as a string", function()
 		assert.is_equal(msg_str, nml.tostring(msg_ud))
@@ -95,9 +128,9 @@ describe ("nml_msg messages #nml_msg", function()
 	--Christian: should we do this? That is, allow making a new message and defining the contents from a string?
 	it("can make a new message from a string.", function()
 		--new empty message
-		msg_ud = nml.new_message("Hello, world.", "string") 
+		msg_ud = nml.nml_msg("Hello, world.", "string") 
 		--string is there
-		assert.is_equal("string", nml.msg_type(msg_ud))
+		assert.is_equal("string", nml.gettype(msg_ud))
 	
 		msg_ud = nil
 		collectgarbage()
